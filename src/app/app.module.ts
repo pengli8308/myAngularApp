cf7f755cb32fd7a0fde358eb415d58d25f13bbe2
Peng Li <pengli8308@gmail.com>
2019-01-07 16:38:41 -0400

    additional changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
+import { MembersComponent } from './members/members.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,UserComponent
+    AppComponent,UserComponent, MembersComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
